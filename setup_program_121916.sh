9f0502915849f75dbf9d6d021090b2319c98c4a9
Priscilla Erickson <priscilla.erickson@gmail.com>
2016-12-20 10:06:34 -0500

    Update and rename setup_program_121416.sh to setup_program_121916.sh

--- setup_program_121916.sh
+++ setup_program_121916.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#prepare sensor test program
 cd ~/RaspberryPiSetup
 chmod 755 sensortest.sh 
+
+#setup automatic time keeping
+sudo cp /usr/share/zoneinfo/America/New_York /etc/localtime
+sudo apt-get install ntp
+sudo sed -i 's/0.debian.pool.ntp.org/ntp1.Virginia.Edu/g; s/1.debian.pool.ntp.org/ntp2.Virginia.Edu/g; s/2.debian.pool.ntp.org/ntp3.Virginia.Edu/g; s/3.debian.pool.ntp.org/ntp4.Virginia.Edu/g' /etc/ntp.conf
+
 #setup the tsl lux detector
 cd ~
 sudo apt-get install i2c-tools libi2c-dev python-dev
@@ -10,8 +17,8 @@ cd tsl2561
 sudo python setup.py install
 sudo sed -i '$a dtparam=i2c1=on' /boot/config.txt
 sudo sed -i '$a dtparam=i2c-arm=on' /boot/config.txt
-#sudo echo 'blacklist snd_bcm2835' >  ~/Desktop/blacklisttest.txt
-sudo echo 'blacklist snd_bcm2835' >  /etc/modprobe.d/snd-blacklist.conf
+sudo echo 'blacklist snd_bcm2835' > /tmp/snd-blacklist.conf
+sudo cp /tmp/snd-blacklist.conf /etc/modprobe.d/snd-blacklist.conf
 
 #setup the neopixels
 cd ~
