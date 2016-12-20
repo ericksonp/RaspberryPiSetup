ef8eff4155dd4db37ed964fd3226163c2bd96894
Priscilla Erickson <priscilla.erickson@gmail.com>
2016-12-20 10:12:23 -0500

    Update setup_program_121916.sh

--- setup_program_121916.sh
+++ setup_program_121916.sh
@@ -6,12 +6,13 @@ chmod 755 sensortest.sh
 
 #setup automatic time keeping
 sudo cp /usr/share/zoneinfo/America/New_York /etc/localtime
-sudo apt-get install ntp
+sudo apt-get --yes --force-yes install ntp
 sudo sed -i 's/0.debian.pool.ntp.org/ntp1.Virginia.Edu/g; s/1.debian.pool.ntp.org/ntp2.Virginia.Edu/g; s/2.debian.pool.ntp.org/ntp3.Virginia.Edu/g; s/3.debian.pool.ntp.org/ntp4.Virginia.Edu/g' /etc/ntp.conf
+sudo /etc/init.d/ntp restart
 
 #setup the tsl lux detector
 cd ~
-sudo apt-get install i2c-tools libi2c-dev python-dev
+sudo apt-get --yes --force-yes install i2c-tools libi2c-dev python-dev
 git clone https://github.com/lexruee/tsl2561.git
 cd tsl2561
 sudo python setup.py install
@@ -22,7 +23,7 @@ sudo cp /tmp/snd-blacklist.conf /etc/modprobe.d/snd-blacklist.conf
 
 #setup the neopixels
 cd ~
-sudo apt-get install build-essential python-dev git scons swig
+sudo apt-get --yes --force-yes install build-essential python-dev git scons swig
 git clone https://github.com/jgarff/rpi_ws281x.git
 cd rpi_ws281x
 scons
@@ -31,7 +32,7 @@ sudo python setup.py install
 
 #setup the code needed for SHT31
 cd ~
-sudo apt-get install build-essential libi2c-dev i2c-tools python-dev libffi-dev
+sudo apt-get install --yes --force-yes build-essential libi2c-dev i2c-tools python-dev libffi-dev
 sudo pip install cffi
 sudo pip install smbus-cffi
 cd ~
@@ -46,5 +47,3 @@ cd Adafruit_Python_MCP9808
 sudo python setup.py install
 
 sudo reboot
-cd RaspberryPiSetup
-sudo ./sensortest.sh
